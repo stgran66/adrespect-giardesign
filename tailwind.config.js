ad9dc7477468e0662814d830289927cf7270cf69
stgran66 <stanislaw.grygoriew@gmail.com>
2023-08-15 23:26:41 +0200

    created header and hero section

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ['Inter', 'sans-serif'],
+        heading: ['Montserrat', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
