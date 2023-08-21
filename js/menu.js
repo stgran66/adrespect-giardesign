de96671dfbdc97bc7bd83edfce334278ea575aab
stgran66 <stanislaw.grygoriew@gmail.com>
2023-08-21 23:09:39 +0200

    updated search bar in header

---
+++ js/menu.js
@@ -0,0 +1,12 @@
+// const refs = {
+//   menuBtn: document.querySelector('#burger-menu'),
+//   navMenu: document.querySelector('#site-nav'),
+//   offerMenuBtn: document.querySelector('#offer-menu-btn'),
+// };
+
+// function toggleOfferMenu() {
+//   console.log('toggle');
+//   this.parentNode.classList.toggle('offer-open');
+// }
+
+// refs.offerMenuBtn.addEventListener('click', toggleOfferMenu);
